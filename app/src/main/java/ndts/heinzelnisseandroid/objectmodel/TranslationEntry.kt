6cab518ae4edffa698acc05298d68dfb23cda28a
ndts <nicolastorres55@gmail.com>
2017-09-15 22:35:42 +0200

    removed expandable listview library

--- app/src/main/java/ndts/heinzelnisseandroid/objectmodel/TranslationEntry.kt
+++ app/src/main/java/ndts/heinzelnisseandroid/objectmodel/TranslationEntry.kt
@@ -9,8 +9,4 @@ class TranslationEntry(private val link: String,
     override fun toString(): String = String.format(
             "Link:\t%s\nGrade:\t%s\nId:\t%d\nOriginal:\n%s\nTranslation:\n%s\n",
             link, grade, id, original.toString(), translation.toString())
-
-    public fun getOriginal(): Word = original
-
-    public fun getTranslation(): Word = translation
 }
--- readme.md
+++ readme.md
@@ -4,4 +4,3 @@ Learn more about the  the [Heinzelnisse REST API](https://www.heinzelnisse.info/
 
 Libraries used in this project:
 - [KillerTask](https://github.com/inaka/KillerTask)
-- [Expandable RecyclerView](https://github.com/thoughtbot/expandable-recycler-view)
